6910582899b676f620e8e7f0cf56772f57ec39b8
gfisk777 <112730593+gfisk777@users.noreply.github.com>
2022-09-23 19:48:17 -0400

    Update script.js

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -7,7 +7,7 @@
 */
 /* eslint-enable max-len */
 // set our first slide's position to "0", the opening position in an array
-const slidePosition = 0;
+let slidePosition = 0;
 
 // gather a reference to every slide we're using via the class name and querySelectorAll
 const slides = document.querySelectorAll('.carousel_item');
@@ -19,6 +19,14 @@ const slidesArray = Array.from(slides);
 const totalSlides = slidesArray.length;
 
 function updateSlidePosition() {
+  slidesArray.forEach((slide) => {
+    slide.classList.remove('visible');
+    slide.classList.add('hidden';
+  });
+  console.log(slidePosition);
+  slides[slidePosition].classList.add('visible');
+  }
+
   // Using the .forEach array method, (array.forEach((element) => { per-element work goes here }))
   // loop through all the slides in your slideArray
   // and remove the 'visible' class from each classList
@@ -26,9 +34,13 @@ function updateSlidePosition() {
 
   // outside your .forEach,
   // add a 'visible' class to the slide at the current slidePosition in slides
-}
 
 function moveToNextSlide() {
+  if (slidePosition === totalSlides -1) {
+    slideposition = 0;
+  } else {
+    slidePosition += 1;
+  }
   /*
     add an if statement here that checks
     if you're already at the max number of slides
@@ -38,13 +50,11 @@ function moveToNextSlide() {
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
-  // add your code in here for when you click the "prev" button
-  /*
-    add an if statement here that checks
-    if you're already at the first index position for an array
-    and if so, sets your slidePosition to the last slide position in totalSlides
-    if not, set the slidePosition to the current position minus one
-  */
+  if (slidePosition === 0) {
+    slidePosition = totalSlides -1;
+  } else {
+    slidePosition -= 1;
+  }
   updateSlidePosition();
 }
 
